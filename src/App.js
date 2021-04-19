664d7a33d050ae56744f97840d9bb59a19c597c3
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-19 10:32:23 -0400

    added div with information for medium article

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+
+  const title = "Welcome to new blog"
+  const likes = 50;
+  const google= "http://www.google.com"
+  //const person = {name: "Mr. Man", age: 30}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times.</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{"Im in side a p tag woohoo"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={google}> away we go!</a>
+      </div>
     </div>
   );
 }

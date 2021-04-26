61b60cc642b22f3ceb3692465b2b3ba18192c47d
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-26 15:57:24 -0400

    added styling

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <h5>
-                Footer fun›
-            </h5>
+            <h3>
+                Footer
+            </h3>
         </div>
     )
 }

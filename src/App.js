e0d8237f70d3badbffea1605b13462e9022d0f4b
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-26 15:42:22 -0400

    added navbar div, contnet div and footer div with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import "./App.css";
+import Footer from "./Footer";
+import Home from "./Home";
+import Navbar from './Navbar'
 
 function App() {
 
-  const title = "Welcome to new blog"
-  const likes = 50;
-  const google= "http://www.google.com"
-  //const person = {name: "Mr. Man", age: 30}
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times.</p>
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>{"Im in side a p tag woohoo"}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={google}> away we go!</a>
+      <Home/>
+      </div>
+      <div className="footer">
+      <Footer/>
       </div>
     </div>
   );

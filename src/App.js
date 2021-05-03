184cd66d2aec02932500defdd069b12e9286749f
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-03 11:17:42 -0400

    added buttons and learned new things

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import Footer from "./Footer";
 import Home from "./Home";
-import Navbar from './Navbar'
+import Navbar from "./Navbar";
 
 function App() {
-
-
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="content">
-      <Home/>
+        <Home />
       </div>
       <div className="footer">
-      <Footer/>
+        <Footer />
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home page</h2>
-        </div>
-    )
-}
+  
 
-export default Home
+  const handleClick = (event) => {
+    console.log("we made it to the click event", event);
+  };
+  const handleClickAgain = (name) => {
+    console.log("welcome to Neverland " + name);
+  };
+  
+ 
+
+  return (
+    <div className="home">
+      <h2>Home page</h2>
+      <button onClick={handleClick}> click me </button>
+      <button
+        onClick={(event) => {
+          handleClickAgain(event, "taco hernadez");
+        }}
+      >
+        {" "}
+        click me 2
+      </button>
+    </div>
+  );
+};
+
+export default Home;

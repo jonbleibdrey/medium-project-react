bb93b233e292e30465e66521dc64e4b29ee3c216
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-10 11:04:09 -0400

    adding output list for react medium blog

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {
-      title: "my new webiste",
+      title: "my new website",
       body:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
       author: "baby jesus",
@@ -25,7 +25,19 @@ const Home = () => {
     },
   ]);
 
-  return <div className="home"></div>;
+  console.log(setBlogs);
+  return (
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>
+            <h4>Written By: {blog.author}</h4>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;

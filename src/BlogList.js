a257dbae3010912ed69785fad9cb2ce52db811dd
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-17 11:26:27 -0400

    switch home map method to bloglist

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const BlogList = ({blogs}) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
+      <h1> {title} </h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
--- src/Home.js
+++ src/Home.js
@@ -5,31 +5,33 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     {
       title: "my new website",
-      body:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
+      body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
       author: "baby jesus",
       id: 1,
     },
     {
       title: "We had a party",
-      body:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
+      body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
       author: "micheal jordan",
       id: 2,
     },
     {
       title: "web dev game",
-      body:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
-      author: "madona",
+      body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolor ullam iste sint cupiditate suscipit! Asperiores sed nam recusandae quo dicta eius natus corporis tempora sapiente, vero aspernatur quidem error?",
+      author: "micheal jordan",
       id: 3,
     },
   ]);
 
   console.log(setBlogs);
+
   return (
     <div className="home">
-     <BlogList blogs={blogs}/>
+      <BlogList blogs={blogs} title="All blogs" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "micheal jordan")}
+        title="Micheal's Blog Post"
+      />
     </div>
   );
 };

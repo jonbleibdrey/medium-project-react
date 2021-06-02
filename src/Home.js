c897fbd67e7bdf3814cbe2dc6b7a2d41b1586707
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-01 20:24:11 -0400

    added a useeffect array dependency called name

--- src/Home.js
+++ src/Home.js
@@ -23,19 +23,26 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("jonathan bleibdrey");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
-  useEffect(()=>{
-    console.log("use effect ran")
-    console.log(blogs)
-  })
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <h1>useeffect example</h1>
+      <button onClick={() => setName("mr. jonathan bleibdrey")}>
+        change name
+      </button>
+      <p>{name}</p>
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "micheal jordan")}
         title="Micheal's Blog Post"

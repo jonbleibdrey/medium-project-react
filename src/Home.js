463751eaad6e3da56395d1b54f7a34a6998577bb
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-14 12:41:29 -0400

    added error handeling

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if(!res.ok){
+          throw Error("Error Message: could not fetch the data")
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
-      });
+        setIsLoading(false)
+        setError(null)
+      })
+      .catch(err => {
+       setError(err.message)
+       setIsLoading(false)
+      })
   }, []);
  
   return (
     <div className="home">
+      { error && <div>{error} </div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>

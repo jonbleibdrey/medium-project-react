f477c3164d44dfad065d43d073f1d6b34de7fda1
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-28 15:26:32 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import React from "react";
 import Footer from "./Footer";
 import Home from "./Home";
+import Create from "./Create";
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/Create.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>
+                add a new blog
+            </h2>
+           
+
+        </div>
+    )
+}
+
+export default Create
+

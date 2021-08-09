47c6809f2f70b6eb07d19c9ccbd2c12ad18f285a
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-09 17:10:58 -0400

    added state to title, body, authour

--- src/Create.js
+++ src/Create.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Create = () => {
-    return (
-        <div className="create">
-            <h2>
-                Add a new blog
-            </h2>
-            <form>
-            <label> Blog title:</label>
-            <input type="text" required/>
-            <label> Blog body:</label>
-            <textarea required>
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("jonathan");
 
-            </textarea>
-            <label> Blog author:</label>
-            <select>
-                <option value="Jonathan"> jonathan</option>
-                <option value="Micheal"> Micheal</option>
-                <option value="yoda"> Baby Yoda</option>
-            </select>
-            <button> Add Blog</button>
-            </form>
-           
+  return (
+    <div className="create">
+      <h2>Add a new blog</h2>
+      <form>
+        <label> Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label> Blog body:</label>
+        <textarea required value={body} onChange={(e) => setBody(e.target.value)} ></textarea>
+        <label> Blog author:</label>
+        <select vlaue={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Jonathan"> jonathan</option>
+          <option value="Micheal"> Micheal</option>
+          <option value="yoda"> Baby Yoda</option>
+        </select>
+        <button> Add Blog</button>
+      </form>
+      <div>
+      <h1> Title: {title} </h1>
+      <h2>  Body: {body} </h2>
+      <h4> Authors name: {author} </h4>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Create
-
+export default Create;

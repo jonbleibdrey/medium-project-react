a3c3a789f87a4323325ab5894723249edaa4bac8
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-09 16:37:30 -0400

    added form

--- src/Create.js
+++ src/Create.js
@@ -6,6 +6,21 @@ const Create = () => {
             <h2>
                 Add a new blog
             </h2>
+            <form>
+            <label> Blog title:</label>
+            <input type="text" required/>
+            <label> Blog body:</label>
+            <textarea required>
+
+            </textarea>
+            <label> Blog author:</label>
+            <select>
+                <option value="Jonathan"> jonathan</option>
+                <option value="Micheal"> Micheal</option>
+                <option value="yoda"> Baby Yoda</option>
+            </select>
+            <button> Add Blog</button>
+            </form>
            
 
         </div>

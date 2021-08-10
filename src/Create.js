07865678fd8684491f60ad6af80afffec2cedf97
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-08-09 20:04:26 -0400

    added post request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,26 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("jonathan");
+  const [isLoading, setIsLoading] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = { title, body, author}
+
+    fetch("http://localhost:8000/blogs", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+    }).then(() => {console.log("new blog added")})
+
+  }
+
+
 
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label> Blog title:</label>
         <input
           type="text"
@@ -19,7 +34,7 @@ const Create = () => {
         <label> Blog body:</label>
         <textarea required value={body} onChange={(e) => setBody(e.target.value)} ></textarea>
         <label> Blog author:</label>
-        <select vlaue={author} onChange={(e) => setAuthor(e.target.value)}>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="Jonathan"> jonathan</option>
           <option value="Micheal"> Micheal</option>
           <option value="yoda"> Baby Yoda</option>
